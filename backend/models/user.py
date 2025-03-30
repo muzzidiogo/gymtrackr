f36a36cf4e778916e28b3d5272e72f38dc9e119b
jfmenezesm <jfmenezesm@gmail.com>
2025-03-30 14:48:05 -0300

    agora ta funcionando porra

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,13 +1,15 @@
-from flask_sqlalchemy import SQLAlchemy
+# models/user.py
+from extensions import db
 
-# Instanciando o banco de dados
-db = SQLAlchemy()
-
-class User(db.Model):
+class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(80), nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(200), nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.nome}>'
+    
+    def to_dict(self):
+        return {
+            "id": self.id, 
+            "nome": self.nome, 
+            "email": self.email
+        }
